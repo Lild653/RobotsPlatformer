f1dd8005faaabc3e5f58e14c2084651f141ca059
Corinne Joireman <corinnejoireman@gmail.com>
2019-09-24 17:56:52 +0200

    Added implementation for coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,47 @@
+﻿//Author: Rinn Joireman
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public float framesPerSecond = 2;
+    public Sprite[] frames;
+    public GameObject coinExplosion;
+    public AudioClip twinkleSound;
+
+
+    private SpriteRenderer spriteRenderer;
+    private float frameTimer;
+    private int currentFrameIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        frameTimer = (1f / framesPerSecond);
+        currentFrameIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // currentFrameIndex++;
+        frameTimer -= Time.deltaTime;
+        if(frameTimer <= 0){
+            currentFrameIndex++;
+            frameTimer = (1f/framesPerSecond);
+            currentFrameIndex = currentFrameIndex%frames.Length;
+            spriteRenderer.sprite = frames[currentFrameIndex];
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        Debug.Log("bitchasssss");
+        AudioSource.PlayClipAtPoint(twinkleSound, transform.position);
+        Instantiate(coinExplosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CoinExplosion.cs
@@ -0,0 +1,40 @@
+﻿//Author: Rinn Joireman
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinExplosion : MonoBehaviour
+{
+    
+    public float framesPerSecond = 2;
+    public Sprite[] frames;
+
+    private SpriteRenderer spriteRenderer;
+    private float frameTimer;
+    private int currentFrameIndex = 0;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        Debug.Log("Hey");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        frameTimer = (1f / framesPerSecond);
+        currentFrameIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // currentFrameIndex++;
+        frameTimer -= Time.deltaTime;
+        if(frameTimer <= 0){
+            currentFrameIndex++;
+            spriteRenderer.sprite = frames[currentFrameIndex];
+            frameTimer = (1f / framesPerSecond);
+        }
+        if(currentFrameIndex == frames.Length - 1){
+            Destroy(gameObject);
+        }
+    }
+}
