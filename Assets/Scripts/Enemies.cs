af1077cb16e16c77f5c0a523d1ec3f3184538b67
Danny <dasimpson@davidson.edu>
2019-09-25 14:48:33 +0200

    New Stuff

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+
+    SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    void playerTracker()
+    {
+        RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, 5f);
+        RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, 5f);
+
+        if (left.collider.CompareTag("Player"))
+        {
+
+        }
+        if (right.collider.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
