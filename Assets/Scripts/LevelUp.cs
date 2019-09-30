41527ef94cd0effe52f1770067ecead548f7409d
Alison Jacobs <alison@jacobsfamilyla.com>
2019-09-30 22:28:01 +0200

    almost got the portal done!

---
+++ Assets/Scripts/LevelUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelUp : MonoBehaviour
+{
+    public string loadlevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print('h');
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(loadlevel);
+        }
+    }
+
+    
+}
