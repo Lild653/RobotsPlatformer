c497adb281c8278242632a997765bdc36474ca57
Liya <17liyaw@gmail.com>
2019-10-02 15:31:36 +0200

    Home Page to Level

---
+++ Assets/Scripts/HomeToLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HomeToLevel : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Liya");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
